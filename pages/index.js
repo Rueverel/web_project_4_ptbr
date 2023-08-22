e09f7ae366eb7d53255b75759c23d2a316e72c01
Rueverel <130900333+Rueverel@users.noreply.github.com>
2023-08-22 16:58:46 +0100

    popup a funcionar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 const modal = document.querySelector(".popup");
+const opaco = document.querySelector(".fade");
 
 function abrirModal () {
   modal.classList.add("popup_opened");
+  opaco.classList.add("fade_on");
 
 }
 
 function fecharModal () {
   modal.classList.remove("popup_opened");
+  opaco.classList.remove("fade_on");
 
 }
