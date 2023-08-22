9518df735ef7bced1ade1e0345f57f3336b20efc
Rueverel <130900333+Rueverel@users.noreply.github.com>
2023-08-22 10:43:16 +0100

    button funciona

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-const button = document.querySelector(".edit_button");
+const modal = document.querySelector(".popup");
 
-const modal = document.querySelector("dialog");
+function abrirModal () {
+  modal.classList.add("popup_opened");
 
-const button_close = document.querySelector(".button_close");
-
-button.onclick = function () {
-  modal.show()
 }
 
-button_close.onclick = function () {
-  modal.close()
+function fecharModal () {
+  modal.classList.remove("popup_opened");
+
 }
