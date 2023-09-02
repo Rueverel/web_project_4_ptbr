cf6edcd6aed559e64a2c84b34ac2dcd45d6cb373
Rueverel <130900333+Rueverel@users.noreply.github.com>
2023-09-02 12:22:01 +0100

    add button abre fecha e estilo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 const modal = document.querySelector(".popup");
+const modalProfile = document.querySelector(".popup__profile-edit");
+const modalPlace = document.querySelector(".popup__place-edit");
 const opaco = document.querySelector(".fade");
 const nomePopup = document.querySelector(".popup__name");
 const aboutPopup = document.querySelector(".popup__aboutme");
 const nomePerfil = document.querySelector(".profile__noum");
 const aboutPerfil = document.querySelector(".profile__hobbie");
 const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button") //teste//
 const saveButton = document.querySelector(".popup__button-save");
+const createButton = document.querySelector(".popup__button-create"); //teste//
 const closeButton = document.querySelector(".popup__button-close");
 
 nomePopup.value = nomePerfil.textContent;
@@ -13,17 +17,24 @@ aboutPopup.value = aboutPerfil.textContent;
 
 
 editButton.addEventListener("click", function(){
-  modal.classList.add("popup_opened");
+  modalProfile.classList.add("popup_opened");
+  opaco.classList.add("fade_on");
+
+});
+
+addButton.addEventListener("click", function(){
+  modalPlace.classList.add("popup_opened");
   opaco.classList.add("fade_on");
 
 });
 
 closeButton.addEventListener("click", function(){
-  modal.classList.remove("popup_opened");
+  modalProfile.classList.remove("popup_opened");
+  modalPlace.classList.remove("popup_opened");
   opaco.classList.remove("fade_on");
 });
 
-modal.addEventListener('submit', (event) => {
+modal.addEventListener('submit', (event) => { // correto??// FALTA BOTAO DE ADICIONAR LOCAL
   event.preventDefault()
   nomePerfil.textContent = nomePopup.value;
   aboutPerfil.textContent = aboutPopup.value;
@@ -31,6 +42,11 @@ modal.addEventListener('submit', (event) => {
 });
 
 saveButton.addEventListener("click", function(){
+  modal.classList.remove("popup_opened");
+  opaco.classList.remove("fade_on");
+});
+
+createButton.addEventListener("click", function(){
   modal.classList.remove("popup_opened");
   opaco.classList.remove("fade_on");
 });
